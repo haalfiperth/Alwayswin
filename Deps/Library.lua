162840c0b945b0736ef6f408b0534ef9c77fd5b5
haalfiperth <ratzzzrich@gmail.com>
2025-03-04 16:39:22 +0700

    Update Library.lua

--- Deps/Library.lua
+++ Deps/Library.lua
@@ -32,7 +32,7 @@ local Library = {
     FontColor = Color3.fromRGB(255, 255, 255);
     MainColor = Color3.fromRGB(28, 28, 28);
     BackgroundColor = Color3.fromRGB(20, 20, 20);
-    AccentColor = Color3.fromRGB(66, 110, 135);
+    AccentColor = Color3.fromRGB(125, 152, 175);
     OutlineColor = Color3.fromRGB(50, 50, 50);
     RiskColor = Color3.fromRGB(255, 50, 50),
 
