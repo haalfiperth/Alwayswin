14f6914fbb5ba2c896f146dd9b0058fb003abac0
haalfiperth <ratzzzrich@gmail.com>
2025-04-11 20:16:27 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/4868ca7e5974414b1fbf79ba6debeb0b.lua"))()
+local DAHOOD_BYPASS = "Full" -- "5 Minutes" or "Full"
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/1cd9f375e3974c9f070ab49219a8f2c2.lua"))()
