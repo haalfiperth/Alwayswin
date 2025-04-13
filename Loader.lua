72532090a3d550c08d1eeca93fb5fe3af9c0a34a
haalfiperth <ratzzzrich@gmail.com>
2025-04-13 20:45:36 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,5 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/1cd9f375e3974c9f070ab49219a8f2c2.lua"))()
+if game.PlaceId == 18668065416 then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/4d1dd73a509298df07896bde20922263.lua"))()
+else
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/1cd9f375e3974c9f070ab49219a8f2c2.lua"))()
+end
