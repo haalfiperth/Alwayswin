098e6739522b5431a952cffacdceb758f5dd0964
haalfiperth <ratzzzrich@gmail.com>
2025-10-10 22:04:41 +0700

    Delete Deps/Bypass.lua

--- Deps/Bypass.lua
+++
@@ -1,7 +0,0 @@
-local OriginalGetFenv; OriginalGetFenv = hookfunction(getrenv().getfenv, newcclosure(function(Level)
-    if not checkcaller() then
-        task.wait(15e15)
-        return {sigma = function() return 0 end, balls = 1, tablehooked = {}}
-    end
-    return OriginalGetFenv(Level)
-end))
